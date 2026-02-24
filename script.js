f78cffcfe3d456f43b0c7765f0a8a07096c7875e
Your Name <shadanakram82@gmail.com>
2026-02-25 01:13:07 +0530

    modifiy pricing section

--- script.js
+++ script.js
@@ -272,3 +272,14 @@ if (featuredPricingCard) {
     featuredPricingCard.style.setProperty('--mouse-y', '50%');
   });
 }
+
+document.querySelectorAll(".pricing-card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  });
+});
